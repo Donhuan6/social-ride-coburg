4eaee0a199cfdbeeb72bd5a3969dd47c72f3de43
Timon <104058661+Donhuan6@users.noreply.github.com>
2026-07-31 18:56:36 +0200

    vercel anstupsen

--- social-ride-coburg_1/social-ride-coburg/src/context/AuthContext.tsx
+++ social-ride-coburg_1/social-ride-coburg/src/context/AuthContext.tsx
@@ -65,4 +65,4 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 // eslint-disable-next-line react-refresh/only-export-components
 export function useAuth() {
   return useContext(AuthContext)
-}        
+}              
